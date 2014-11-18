37722ad6ff60368be3828cbaab10180b41a0f161
Jonathan Reem <jonathan.reem@gmail.com>
2014-11-18 01:00:59 -0800

    (feat) Added identity-like impl for Vec<callback>.

--- src/impls.rs
+++ src/impls.rs
@@ -3,3 +3,9 @@ use {Reduce};
 
 include!(concat!(env!("OUT_DIR"), "/impls.rs"))
 
+impl<'a, I> Reduce<I, I> for Vec<Box<Callback<I, I> + 'a>> {
+    fn reduce(self, input: I) -> I {
+        self.into_iter().fold(input, |val, fun| fun.invoke(val))
+    }
+}
+
